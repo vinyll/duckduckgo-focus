cbbc3af3b7cb49f318576fb16e8f6e59fd5f385d
Vincent Agnano <vinyll@protonmail.com>
2022-02-26 10:19:27 +0100

    remove ads from main content and sidebar

---
+++ ddg-focus.js
@@ -0,0 +1,9 @@
+const topAds = document.getElementById('ads')
+if(topAds) {
+  topAds.setAttribute('hidden', true)
+}
+
+const sidebarAds = document.querySelector('.js-sidebar-ads')
+if(sidebarAds) {
+  sidebarAds.setAttribute('hidden', true)
+}
